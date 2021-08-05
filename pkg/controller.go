243528c87316756f5c8534f80d6aeb5f46312d47
Nikita <kinitaleino@gmail.com>
2021-08-05 15:47:33 +0500

    controller changed

--- pkg/controller.go
+++ pkg/controller.go
@@ -4,9 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"log"
-	"math/rand"
 	"net/http"
-	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -22,37 +20,29 @@ func GetNames(w http.ResponseWriter, r *http.Request) {
 
 	var people []Person
 
-	db, err := sql.Open("mysql",
-		"root:root@tcp(localhost:6033)/app_db")
-	if err != nil {
-		log.Fatal(err)
-	}
+	db := connectToDB()
 	defer db.Close()
+
 	stmt, err := db.Prepare("select * from users")
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 	defer stmt.Close()
+
 	rows, err := stmt.Query()
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 	defer rows.Close()
+
 	for rows.Next() {
 		/*rows.Next() что-то вроде foreach*/
 		var person Person
 		err := rows.Scan(&person.ID, &person.Name)
 		/*rows.Scan() записывает из строки БД в переменные*/
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkError(err)
 		people = append(people, person)
 		//log.Println(person.ID, person.Name)
 	}
 	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
+
 	json.NewEncoder(w).Encode(people)
 }
 
@@ -60,35 +50,28 @@ func GetName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	var person Person
-	db, err := sql.Open("mysql",
-		"root:root@tcp(localhost:6033)/app_db")
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	db := connectToDB()
 	defer db.Close()
+
 	stmt, err := db.Prepare("select id, name from users where id = ?")
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 	defer stmt.Close()
+
 	rows, err := stmt.Query(params["id"])
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 	defer rows.Close()
+
 	for rows.Next() {
 		/*rows.Next() что-то вроде foreach*/
 		err := rows.Scan(&person.ID, &person.Name)
 		/*rows.Scan() записывает из строки БД в переменные*/
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkError(err)
 		//log.Println(person.ID, person.Name)
 	}
 	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
+
 	json.NewEncoder(w).Encode(person)
 }
 
@@ -96,31 +79,39 @@ func CreateName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var person Person
 	_ = json.NewDecoder(r.Body).Decode(&person)
-	person.ID = strconv.Itoa(rand.Intn(1000))
 
+	db := connectToDB()
+	defer db.Close()
+
+	stmt, err := db.Prepare("INSERT INTO users(name) VALUES (?)")
+	checkError(err)
+	defer stmt.Close()
+
+	res, err := stmt.Exec(person.Name)
+	checkError(err)
+
+	lastId, err := res.LastInsertId()
+	checkError(err)
+
+	rowCnt, err := res.RowsAffected()
+	checkError(err)
+
+	log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+
+	json.NewEncoder(w).Encode(person)
+}
+
+func connectToDB() *sql.DB {
 	db, err := sql.Open("mysql",
 		"root:root@tcp(localhost:6033)/app_db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-	stmt, err := db.Prepare("INSERT INTO users(id,name) VALUES (?,?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	res, err := stmt.Exec(person.ID, person.Name)
-	if err != nil {
-		log.Fatal(err)
-	}
-	lastId, err := res.LastInsertId()
-	if err != nil {
-		log.Fatal(err)
-	}
-	rowCnt, err := res.RowsAffected()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+	return db
+}
 
-	json.NewEncoder(w).Encode(person)
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
